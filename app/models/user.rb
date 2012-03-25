9e5d0be26d00f25e93544e47d7d13e92c49a4198
Daniel Brajkovic <dnaiel@brajkovic.com>
2012-03-24 22:02:41 -0500

    Invitable and token_authenticable added. Not fully implemented yet.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   ## Database authenticatable
   field :email,              :type => String, :null => false, :default => ""
@@ -27,18 +27,26 @@ class User
   # field :password_salt, :type => String
 
   ## Confirmable
-  # field :confirmation_token,   :type => String
-  # field :confirmed_at,         :type => Time
-  # field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
+  field :confirmation_token,   :type => String
+  field :confirmed_at,         :type => Time
+  field :confirmation_sent_at, :type => Time
+  field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
   ## Lockable
   # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
   # field :locked_at,       :type => Time
 
+  ## Invitable
+  field :invitation_token,     :limit => 60
+  field :invitation_sent_at
+  field :invitation_token
+  field :invitation_limit
+  field :invited_by_id
+  field :invited_by_type
+
   ## Token authenticatable
-  # field :authentication_token, :type => String
+  field :authentication_token, :type => String
   field :name
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,11 +24,11 @@ MousehouseRails::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "dbrajkovic@gmail.com", #ENV["GMAIL_USERNAME"],
+    password: "dB072170" #ENV["GMAIL_PASSWORD"]
   }
 
 
