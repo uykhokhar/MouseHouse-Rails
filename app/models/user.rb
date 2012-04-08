a6ae04e4070623622e4cc369f2d53cad9a35555d
Daniel Brajkovic <daniel@brajkovic.com>
2012-04-07 19:02:13 -0500

    Turned off token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
   field :email,              :type => String, :null => false, :default => ""
